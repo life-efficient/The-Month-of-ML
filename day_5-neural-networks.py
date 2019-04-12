b218e29a59ffd52013dccc6fba119e6f2387c356
Harry Berg <34554812+life-efficient@users.noreply.github.com>
2019-04-12 23:59:37 +0100

    Add files via upload

---
+++ day_5-neural-networks.py
@@ -0,0 +1,98 @@
+import torch
+import numpy as np
+from torch.utils.data import Dataset, DataLoader
+import matplotlib.pyplot as plt
+
+X = np.linspace(-10, 10)
+Y = X**2 + 10*np.sin(X)
+
+fig = plt.figure(figsize=(10, 5))
+h_ax = fig.add_subplot(121)
+loss_ax = fig.add_subplot(122)
+
+print(X, Y)
+h_ax.plot(X, Y)
+h_ax.set_ylim(-10, 50)
+
+X = torch.tensor(X).float().unsqueeze(1)
+Y = torch.tensor(Y).float().unsqueeze(1)
+print(X)
+
+
+plt.ion()
+plt.show()
+
+epochs = 10
+batch_size = 24
+
+class MyDataset(Dataset):
+
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+
+    def __getitem__(self, idx):                           # what to do when the instance is indexed with square brackets
+        x = self.features[idx]                            # get features at that index
+        x = torch.tensor(x)                               # convert numpy array to torch tensor so gradients are
+        x = x.unsqueeze(0)
+        y = self.labels[idx]
+        y = torch.tensor(y)
+        y = y.unsqueeze(0)
+        return x, y
+
+    def __len__(self):
+        return len(self.features)                             # length of dataset is length of features
+
+dataset = MyDataset(X, Y)                                                       # create an instance of the dataset
+
+dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)           # dataloaders batch and shuffle the dataset for us
+
+class NN(torch.nn.Module):
+    def __init__(self):
+        super().__init__()                                  # initialise the parent class
+        ac = torch.nn.Sigmoid                               # define the actuvation function you want to use here (dont create it by calling it)
+        self.layers = torch.nn.Sequential(
+            torch.nn.Linear(1, 30),                         # linear layer from 1 unit to 50 (make 50 linear combinations of this input)
+            ac(),                                           # create the activation function at this layer
+            torch.nn.Linear(30, 50),                        # hidden layer
+            ac(),
+            torch.nn.Linear(50, 1)                          # output layer (50 units -> single scalar output prediction)
+        )
+
+    def forward(self, x):
+        return self.layers(x)                               # forward pass
+
+nn = NN()
+
+optimiser = torch.optim.SGD(nn.parameters(), lr=0.01)           # create stochastic gradient descent optimiser
+criterion = torch.nn.MSELoss()                                  # create function to
+
+epochs = 100
+
+losses = []
+
+for epoch in range(epochs):                                                         # how many times to go through whole dataset?
+    for batch_idx, batch in enumerate(dataloader):                                  # for each minibatch from dataloader
+        print(batch)
+
+        x, y = batch                                                               # unpack the minibatch
+        h = nn(x)                                                                  # make predictions for this minibatch
+        loss = criterion(h, y)                                                     # evaluate loss for this batch
+        loss.backward()                   # differentiate loss with respect to parameters that the optimiser is tracking
+
+        optimiser.step()                                                            # take optimisation step
+        optimiser.zero_grad()                                                       # set parameter gradients = 0 (otherwise they accumulate)
+
+        # PURELY FOR PLOTTING THE HYPOTHESIS OVER THE WHOLE INPUT DOMAIN
+        all_preds = nn(X).detach().numpy()                  # make predictions for all inputs (not just minibatch)
+        h_plot = h_ax.plot(X.numpy(), all_preds, c='g')    # plot predictions for all inputs
+
+        print(f'Epoch: {epoch} \t\t Batch: {batch_idx} \t\tLoss: {loss.item()}')
+
+        losses.append(loss.item())
+        loss_ax.plot(losses, 'g')
+
+
+        plt.pause(0.1)                                      # pause to give us time to view the hypothesis plot
+        h_plot.pop(0).remove()                              # remove the previous plot
+
